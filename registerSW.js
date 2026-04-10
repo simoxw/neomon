60152f1262edd7b7e495e0ac088979f51b036e8d
simoxw <enomisrasalgul@gmail.com>
2026-04-10 17:22:51 +0200

    sw 2

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/neomon/sw.js', { scope: '/neomon/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
